aa83d28e6464c7ab117ab195c19cea78b3e6c27f
Imrazina <annasabossova@gmail.com>
2025-03-19 17:57:40 +0100

    add login

---
+++ src/main/java/dreamteam/com/chatplatform/config/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package dreamteam.com.chatplatform.config;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import dreamteam.com.chatplatform.model.UserEntity;
+import dreamteam.com.chatplatform.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER") // Можно добавить роли из БД
+                .build();
+    }
+}
